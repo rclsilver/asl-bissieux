ca6de724ed072c903af1f065f58a83470a16c879
Thomas Bétrancourt <thomas@betrancourt.net>
2024-05-14 21:36:48 +0200

    fix generate-openapi.sh: use docker-compose or docker compose

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -4,4 +4,8 @@ set -eux
 
 curl --fail -o ./frontend/openapi.json http://localhost:8080/api/spec.json
 
-docker-compose exec -u node -w /app frontend npm run openapi
+if [ -x docker-compose ]; then
+    docker-compose exec -u node -w /app frontend npm run openapi
+else
+    docker compose exec -u node -w /app frontend npm run openapi
+fi
